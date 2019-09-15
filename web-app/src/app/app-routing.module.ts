bdb96c0472ed202ff79dc3ea681a2c46930adbea
venkatesh-buddy <venkatesh88a@gmail.com>
2019-09-15 19:47:37 +0530

    Modified

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExtraContainerComponent } from './extra-container/extra-container.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path : 'weather', pathMatch : 'full', component: ExtraContainerComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
